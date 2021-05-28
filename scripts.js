5f83ee13f382597450a04594bb6237ac17fb004c
Merison442 <messin.santos12@gmail.com>
2021-05-28 11:24:45 -0300

    JavaScript inserido

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+
+// Comentario multi linha (comentário de bloco) = /* */
+/* "" // aspas duplas
+'' // aspas simples
+`` // template literals ou template string */
+
+
+    console.log(`Bem Vindos ao Start`)
+    
