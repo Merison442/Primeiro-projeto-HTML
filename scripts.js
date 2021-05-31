b60d41c03b7d0e27b22e6ca76240ccae33d676ed
Merison442 <messin.santos12@gmail.com>
2021-05-31 12:03:45 -0300

    comentários adicionados

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 
 
 // Comentario multi linha (comentário de bloco) = /* */
-/* "" // aspas duplas
-'' // aspas simples
-`` // template literals ou template string */
+// "" aspas duplas.
+// '' aspas simples.
+// `` template literals ou template string.
 
+// Boolean -true
+//         -false
 
-    console.log(`Bem Vindos ao Start`)
-    
+// indefined- indefinido, algo que não vai existir.
+// null- nulo, algo que existe porém não há nada dentro.
+
+// Object- propriedades e funcionalidades
+// { propriedade: "valor"}
+
+console.log(33)
