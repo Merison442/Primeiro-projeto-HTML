c50b03ca8b6548ac5905a8cfd5eb4ad4a3a29847
Merison442 <messin.santos12@gmail.com>
2021-06-10 12:35:45 -0300

    passei o código pra cá

--- scripts.js
+++ scripts.js
@@ -11,8 +11,8 @@
 // indefined- indefinido, algo que não vai existir.
 // null- nulo, algo que existe porém não há nada dentro.
 
-// Object- propriedades e funcionalidades
-// { propriedade: "valor"}
+// Object - propriedades e funcionalidades
+// propriedade: "valor" = {}
 
 // Array []
 // Uma lista
@@ -37,3 +37,47 @@ isHuman = true
 // var é global e local, e poderá ser usada fora de um escopo de bloco. 
 // escopo- determina a visibilidade de alguma variável no JS
 // Block statement- sempre cria um novo escopo
+
+
+
+
+
+let weight
+weight = 66
+// console.log(typeof weight)
+
+// let name, age, stars, isSubscribed
+
+// name = Biny
+// age = 17
+// stars = 0.5
+// isSubscribed = false
+
+let student = {
+    name: "Mérison",
+    age: 18,
+    weight: 66.87,
+    isSubscribed: false,
+};
+    // console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg.`)
+    
+    let students = []
+
+    students = [ 
+    student
+]
+console.log(students[0])
+
+const biny = {
+    name: "biny",
+    age: 17,
+    weight: 58.7,
+    isSubscribed: true
+}
+students[1] = biny
+console.log(students)
+
+
+
+
+
